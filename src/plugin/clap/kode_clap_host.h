1137399557648b1f3535aa6d99d667775da78d09
tor.helge.skei <tor.helge.skei@gmail.com>
2021-11-29 00:31:04 +0100

    added host extensions methods

--- src/plugin/clap/kode_clap_host.h
+++ src/plugin/clap/kode_clap_host.h
@@ -112,6 +112,202 @@ public:
   }
 
 //------------------------------
+public: // extensions
+//------------------------------
+
+  //--------------------
+  // audio-ports-config
+  //--------------------
+
+  // Rescan the full list of configs.
+  // [main-thread]
+
+  void audio_ports_config_rescan() {
+    MHostAudioPortsConfig->rescan(MClapHost);
+  }
+
+  //--------------------
+  // audio-ports
+  //--------------------
+
+  // [main-thread]
+
+  uint32_t audio_ports_get_preferred_sample_size() {
+    return MHostAudioPorts->get_preferred_sample_size(MClapHost);
+  }
+
+  // Rescan the full list of audio ports according to the flags.
+  // [main-thread]
+
+  void audio_ports_rescan(uint32_t flags) {
+    MHostAudioPorts->rescan(MClapHost,flags);
+  }
+
+  //--------------------
+  // event-filter
+  //--------------------
+
+  void event_filter_changed() {
+    MHostEventFilter->changed(MClapHost);
+  }
+
+  //--------------------
+  // fd-support
+  //--------------------
+
+  // [main-thread]
+
+  bool fd_support_register_fd(clap_fd fd, clap_fd_flags flags) {
+    return MHostFdSupport->register_fd(MClapHost,fd,flags);
+  }
+
+  // [main-thread]
+
+  bool fd_support_modify_fd(clap_fd fd, clap_fd_flags flags) {
+    return MHostFdSupport->modify_fd(MClapHost,fd,flags);
+  }
+
+  // [main-thread]
+
+  bool fd_support_unregister_fd(clap_fd fd) {
+    return MHostFdSupport->unregister_fd(MClapHost,fd);
+  }
+
+  //--------------------
+  // gui
+  //--------------------
+
+  /*
+    Request the host to resize the client area to width, height.
+    Return true on success, false otherwise.
+     [thread-safe]
+  */
+
+  bool gui_resize(uint32_t width, uint32_t height) {
+    return MHostGui->resize(MClapHost,width,height);
+  }
+
+  //--------------------
+  // latency
+  //--------------------
+
+  // Tell the host that the latency changed.
+  // The latency is only allowed to change if the plugin is deactivated.
+  // If the plugin is activated, call host->request_restart()
+  // [main-thread]
+
+  void latency_changed() {
+    MHostLatency->changed(MClapHost);
+  }
+
+  //--------------------
+  // log
+  //--------------------
+
+  // Log a message through the host.
+  // [thread-safe]
+
+  void log(clap_log_severity severity, const char *msg) {
+    MHostLog->log(MClapHost,severity,msg);
+  }
+
+  //--------------------
+  // note-name
+  //--------------------
+
+  // Informs the host that the note names has changed.
+  // [main-thread]
+
+  void note_name_changed() {
+    MHostNoteName->changed(MClapHost);
+  }
+
+  //--------------------
+  // params
+  //--------------------
+
+  // Rescan the full list of parameters according to the flags.
+  // [main-thread]
+
+  void params_rescan(clap_param_rescan_flags flags) {
+    MHostParams->rescan(MClapHost,flags);
+  }
+
+  // Clears references to a parameter
+  // [main-thread]
+
+  void params_clear(clap_id param_id, clap_param_clear_flags flags) {
+    MHostParams->clear(MClapHost,param_id,flags);
+  }
+
+  // Request the host to call clap_plugin_params->fush().
+  // This is useful if the plugin has parameters value changes to report to the host but the plugin
+  // is not processing.
+  // eg. the plugin has a USB socket to some hardware controllers and receives a parameter change
+  // while it is not processing.
+  // This must not be called on the [audio-thread].
+  // [thread-safe]
+
+  void params_request_flush() {
+    MHostParams->request_flush(MClapHost);
+  }
+
+  //--------------------
+  // state
+  //--------------------
+
+  // Tell the host that the plugin state has changed and should be saved again.
+  // If a parameter value changes, then it is implicit that the state is dirty.
+  // [main-thread]
+
+  void state_mark_dirty() {
+    MHostState->mark_dirty(MClapHost);
+  }
+
+  //--------------------
+  // thread-check
+  //--------------------
+
+  /*
+    This interface is useful to do runtime checks and make
+    sure that the functions are called on the correct threads.
+    It is highly recommended to implement this extension
+  */
+
+  //----------
+
+  // Returns true if "this" thread is the main thread.
+  // [thread-safe]
+
+  bool thread_check_is_main_thread() {
+    return MHostThreadCheck->is_main_thread(MClapHost);
+  }
+
+  // Returns true if "this" thread is one of the audio threads.
+  // [thread-safe]
+
+  bool thread_check_is_audio_thread() {
+    return MHostThreadCheck->is_audio_thread(MClapHost);
+  }
+
+  //--------------------
+  // timer-support
+  //--------------------
+
+  // Registers a periodic timer.
+  // The host may adjust the period if it is under a certain threshold.
+  // 30 Hz should be allowed.
+  // [main-thread]
+
+  bool timer_support_register_timer(uint32_t period_ms, clap_id *timer_id) {
+    return MHostTimerSupport->register_timer(MClapHost,period_ms,timer_id);
+  }
+
+  // [main-thread]
+
+  bool timer_support_unregister_timer(clap_id timer_id) {
+    return MHostTimerSupport->unregister_timer(MClapHost,timer_id);
+  }
 
 };
 
